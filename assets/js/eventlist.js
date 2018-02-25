975ca1f1a5698fad60a12e215b18a66dc39fdcf2
RISHAV1upes <rishavbits2016@gmail.com>
2018-02-25 16:51:27 +0530

    Add files via upload

---
+++ assets/js/eventlist.js
@@ -0,0 +1,17 @@
+var array = new Array();
+array[0] = {name:'Avartan',img:'avartan',subevent : [['Indus Solva','indussolva'],['Agnikund','agnikund'],['Metal Trivia','metaltrivia'],['Chitrankit','chitrankit'],['Funtoosh','funtoosh']]};
+array[1] = {name:'SciQMetry and Exposicion',img:'sciqmetry',subevent : [['SciQMetry','sciqmetryevent'], ['Exposicion', 'exposicion']]};
+array[2] = {name:'Circuit House',img:'circuithouse',subevent : [['HVC','hvc'],['Elixir Of Electricity','elixirofelectricity'],['Pro-Lo-Co','proloco'],['Mat-sim','matsim'],['Nexus','nexus'],['Electro Q','electroq'], ['Embetrix', 'embetrix']]};
+array[3] = {name:'Armageddon',img:'armageddon',subevent : [['Fifa','fifa'],['CS','cs'],['NFS','nfs'],['Angry Birds','angrybirs'],['Dota','dota'], ['Age of Empires', 'ageofempires']]};
+array[4] = {name:'Kalp It',img:'kalpit',subevent : [['App-Droid','appdroid'],['Codathon','codathon'],['CodeMania','codemania'],['Code Your Bot','codeyourbot'],['Decode','decode'],['Tux','tux'],['Web Vogue','webvogue']]};
+array[5] = {name:'Prayas',img:'prayas',subevent : [['Jagriti','Jagriti'],['Samvedna','samvedna'],['Pratibimb','pratibimb']]};
+array[6] = {name:'Silicon Valley',img:'siliconvalley',subevent : [['Tukvilla','tukvilla'],['Jigyasa','jigyasa'],['The Pixel Game','thepixelgame'],['CodeSense','codesense'],['Analog Hunter','analoghunter'],['Digizone','digizone'],['Netkraft','netkraft']]};
+array[7] = {name:'No Ground Zone',img:'nogroundzone',subevent : [['MICAV','micav'],['RC Plane','rcplane']]};
+array[8] = {name:'Deus-X-Machina',img:'deusxmachina',subevent : [['360mania','360mania'],['Vic-Toy-Rie','victoyrie'],['Battleship','Battleship'],['Kurukshetra','kurukshetra'],['Robo Soccer','robosoccer'],['Shape Shifter','shapeshifter']]};
+array[9] = {name:'Paraphernalia',img:'paraphernalia',subevent : [['The Great Ojass Race','thegreatojassrace'],['Si-fi','sifi'],['Mad Ad','madad'],['Lens View','lensview'],['Directors Cut','directorscut']]};
+array[10] = {name:'NSCET',img:'nscet',subevent : [['NSCET','eventnscet']]};
+array[11] = {name:'Arthashashtra',img:'arthashashtra',subevent : [['ABC','abc'],['Neetishashtra','neetishashtra'],['Lets Start up','letsstartup'],['Corporate Roadies','corporateroadies'],['Bizzathlon','bizzathlon'],['Wolf of Dalal Street','wolfofdalalstreet']]};
+array[12] = {name:'Produs',img:'piethrone',subevent : [['Industrial Tycoon','industrialtycoon'],['Utpreaks','utpreaks'],['Artifact','artifact'],['Drongyan','drongyan'],['Udyog','udyog']]};
+array[13] = {name:'Rise of Machines',img:'riseofmachines',subevent : [['Auto Quiz','autoquiz'],['Acceledrome','acceledrome'],['Moto Virtuale','motovirtuale'],['Samveg','Samveg'],['Junkyards Wars','junkyardwars'],['Prakshepan','prakshepan'],['Enigma','enigma']]};
+array[14] = {name:'Neo Drishti',img:'neodrishti',subevent : [['codiyapa','codiyapa'],['Game Of Troves','gameoftroves'],['Scratch Easy','scratcheasy'],['Simply Sql','simplysql'],['Codigo','web2']]};
+array[15] = {name:'Aakriti',img:'aakriti',subevent : [['Acumen','accumen'],['Sanrachna','sanrachna'],['Archmadeease','archmadeease'],['Exemplar','Exemplar'],['Pipeomania','Pipeomania'],['Metropolis','metropolis']]};
